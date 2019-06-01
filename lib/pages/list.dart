09e2b012dccc554592b5bbd1560a73484639dfd2
Michell Hornung <michellhornung@gmail.com>
2019-06-01 14:04:42 -0300

    Navigation with pages structure

---
+++ lib/pages/list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map_boiler_plate/pages/map.dart';
+
+class ListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Map'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MapPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
