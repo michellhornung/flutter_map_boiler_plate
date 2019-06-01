92933dc961b2d1a2ca1d939df7997a60e298da80
Michell Hornung <michellhornung@gmail.com>
2019-06-01 15:58:58 -0300

    Update list file

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -6,19 +6,24 @@ class ListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Route'),
+        title: Text('List Page'),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Map'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MapPage()),
-            );
-          },
+      body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
         ),
-      ),
     );
   }
 }
