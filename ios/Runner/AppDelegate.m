a091b78c23d1daee8c8e44c4e6eda0629672504a
Jorge Madson <filholouco@gmail.com>
2019-06-01 15:56:02 -0300

    YOURKEYHERE in the right place

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,7 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"AIzaSyAZxZLkKXwciL-20I3CspijlbNP8nTJN7Q"];
+  [GMSServices provideAPIKey:@"YOURKEYHERE"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
