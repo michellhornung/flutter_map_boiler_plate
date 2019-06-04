54ff16f7e6210b91681e245cb08d1fad391bbba8
Michell Hornung <michellhornung@gmail.com>
2019-06-04 12:11:26 -0300

    New list

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -5,25 +5,56 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('List Page'),
-      ),
-      body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.map),
-              title: Text('Map'),
+        appBar: AppBar(
+          title: Text('List Page'),
+        ),
+        body: ListView(
+          children: const <Widget>[
+            Card(child: ListTile(title: Text('One-line ListTile'))),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with leading widget'),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.photo_album),
-              title: Text('Album'),
+            Card(
+              child: ListTile(
+                title: Text('One-line with trailing widget'),
+                trailing: Icon(Icons.more_vert),
+              ),
             ),
-            ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('Phone'),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(),
+                title: Text('One-line with both widgets'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text('One-line dense ListTile'),
+                dense: true,
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 56.0),
+                title: Text('Two-line ListTile'),
+                subtitle: Text('Here is a second line'),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 72.0),
+                title: Text('Three-line ListTile'),
+                subtitle:
+                    Text('A sufficiently long subtitle warrants three lines.'),
+                trailing: Icon(Icons.more_vert),
+                isThreeLine: true,
+              ),
             ),
           ],
-        ),
-    );
+        ));
   }
 }
